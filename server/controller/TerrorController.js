6eb84a7c6a1e3c8ad18c367db3b0c9aaffa7a2c4
noatendler <noatendler1@gmail.com>
2017-01-26 22:27:08 +0200

    display by date

--- server/controller/TerrorController.js
+++ server/controller/TerrorController.js
@@ -7,6 +7,20 @@ terror = require('../models/graphSchema');
 
 exports.getData = function(req, res){
     terror.find({topic:'Terror'},function(err, docs){
+            
+ function sortByFirstName(a, b) {
+    var sortStatus = 0;
+ 
+    if (a.date < b.date) {
+        sortStatus = -1;
+    } else if (a.date > b.date) {
+            sortStatus = 1;
+    }
+    return sortStatus;
+}
+ 
+docs.sort(sortByFirstName);
+
         console.log("docs "+docs);
         res.json(docs);
     });
