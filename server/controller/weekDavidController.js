c0abff68d22d23203ebe42dca6e24a9b8adb3c32
noatendler <noatendler1@gmail.com>
2017-01-26 22:31:09 +0200

    display by date

--- server/controller/weekDavidController.js
+++ server/controller/weekDavidController.js
@@ -12,13 +12,27 @@ exports.getData = function(req, res){
         if (err){
               throw err;
         }
-  
+
+      function sortByFirstName(a, b) {
+          var sortStatus = 0;
+       
+          if (a.date < b.date) {
+              sortStatus = -1;
+          } else if (a.date > b.date) {
+                  sortStatus = 1;
+          }
+          return sortStatus;
+      }
+      docs.sort(sortByFirstName);
      t1 = docs; 
 
         davidCelebrity.find({topic:'celebrity death1'},function(error, docs){
             if (err){
                   throw err;
             }
+
+    docs.sort(sortByFirstName);
+
    
      t2 = docs; 
 
